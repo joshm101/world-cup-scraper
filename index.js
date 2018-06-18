cef51bef1cab1472beba62e7e2244cc1e7043196
Joshua Mora <jshm101@gmail.com>
2018-06-18 00:39:11 -0700

    scraper entry point initDatabase() utilization

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const config = require('config')
+
 const getGroupStageMatches = require('./scripts/getGroupStageMatches')
 const getGroups = require('./scripts/getGroups')
+const initDatabase = require('./scripts/initDatabase')
 
 const app = () => {
-  getGroupStageMatches()
-  getGroups()
+  if (config.INIT_DB) {
+    try {
+      initDatabase()
+    } catch (error) {
+      console.error('-- DB initialization error --')
+      console.error(error)
+    }
+  }
 }
 
 module.exports = app
