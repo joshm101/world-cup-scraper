ae9c8fd094172c0bd056183834e5d0e7208ad1b3
Joshua Mora <jshm101@gmail.com>
2018-06-18 17:59:41 -0700

    update Match schema so that each team's goals are derived properties

--- models/Match/index.js
+++ models/Match/index.js
@@ -6,11 +6,27 @@ const MatchSchema = new Schema({
   date: Date,
   homeTeam: ObjectId,
   awayTeam: ObjectId,
-  homeTeamGoals: Number,
-  awayTeamGoals: Number,
   homeTeamGoalsByHalf: [Number],
   awayTeamGoalsByHalf: [Number]
 })
+
+/**
+ * Derives the total home team goals from
+ * homeTeamGoalsByHalf
+ * @return {number} - total home team goals
+ */
+MatchSchema.method.homeTeamGoals = function() {
+  return this.homeTeamGoalsByHalf[0] + this.homeTeamGoalsByHalf[1]
+}
+
+/**
+ * Dervies the the total away team goals
+ * from awayTeamGoalsByHalf
+ * @return {number} - total away team goals
+ */
+MatchSchema.method.awayTeamGoals = function() {
+  return this.awayTeamGoalsByHalf[0] + this.awayTeamGoalsByHalf[1]
+}
 
 const Match = mongoose.model('Match', MatchSchema)
 
