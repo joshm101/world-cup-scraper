1803a7d150f7155c08b082b6145f2a170ab104f5
Joshua Mora <jshm101@gmail.com>
2018-06-20 13:00:15 -0700

    run containers in detached mode

--- run-init-db.sh
+++ run-init-db.sh
@@ -1,2 +1,5 @@
+docker kill init-db-container
+docker rm init-db-container
 docker build -t init-db --build-arg WCS_MONGODB_CONNECTION_URI=${WCS_MONGODB_CONNECTION_URI} --build-arg WCS_MONGODB_CONNECTION_USERNAME=${WCS_MONGODB_CONNECTION_USERNAME} --build-arg WCS_MONGODB_CONNECTION_PASSWORD=${WCS_MONGODB_CONNECTION_PASSWORD} --build-arg INIT_DB=true .
-docker run init-db
+docker run --name init-db-container init-db
+docker logs -f init-db-container
--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
+docker kill match-scraper-container
+docker rm match-scraper-container
 docker build -t match-scraper --build-arg WCS_MONGODB_CONNECTION_URI=${WCS_MONGODB_CONNECTION_URI} --build-arg WCS_MONGODB_CONNECTION_USERNAME=${WCS_MONGODB_CONNECTION_USERNAME} --build-arg WCS_MONGODB_CONNECTION_PASSWORD=${WCS_MONGODB_CONNECTION_PASSWORD} .
-docker run match-scraper
+docker run -d --name match-scraper-container match-scraper
+docker logs -f match-scraper-container
