7ea433c9c8bd49a4872aee9a3159bdab0e47989a
Joshua Mora <jshm101@gmail.com>
2018-06-26 23:24:13 -0700

    remove unneeded model methods

--- models/Match/index.js
+++ models/Match/index.js
@@ -11,24 +11,6 @@ const MatchSchema = new Schema({
   inProgress: Number
 })
 
-/**
- * Derives the total home team goals from
- * homeTeamGoalsByHalf
- * @return {number} - total home team goals
- */
-MatchSchema.method.homeTeamGoals = function() {
-  return this.homeTeamGoalsByHalf[0] + this.homeTeamGoalsByHalf[1]
-}
-
-/**
- * Dervies the the total away team goals
- * from awayTeamGoalsByHalf
- * @return {number} - total away team goals
- */
-MatchSchema.method.awayTeamGoals = function() {
-  return this.awayTeamGoalsByHalf[0] + this.awayTeamGoalsByHalf[1]
-}
-
 const Match = mongoose.model('Match', MatchSchema)
 
 module.exports = Match
